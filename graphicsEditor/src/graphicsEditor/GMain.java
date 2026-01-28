80ac3f7a712c1838acb2c75ad112f2681b974c3a
chois <chois@Samsung_2024>
2026-01-28 17:40:39 +0900

    20260128

---
+++ graphicsEditor/src/graphicsEditor/GMain.java
@@ -0,0 +1,18 @@
+package graphicsEditor;
+
+public class GMain {
+	public GMain() {
+		
+	}
+	
+	private void initialize() {
+		
+	}
+
+	public static void main(String[] args) {
+		GMain main = new GMain();
+		main.initialize();
+	}
+
+
+}
